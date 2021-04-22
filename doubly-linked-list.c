7da3435900afee556daa0d2d417cc945d9af80a7
mwj1205 <mwj1205@naver.com>
2021-04-22 15:29:03 +0900

    init&inlast

--- doubly-linked-list.c
+++ doubly-linked-list.c
@@ -31,12 +31,12 @@ typedef struct Head {
 /* 함수 리스트 */
 
 /* note: initialize는 이중포인터를 매개변수로 받음
-         singly-linked-list의 initialize와 차이점을 이해 할것 */
+		  singly-linked-list의 initialize와 차이점을 이해 할것 */
 int initialize(headNode** h);
 
 /* note: freeList는 싱글포인터를 매개변수로 받음
-        - initialize와 왜 다른지 이해 할것
-        - 이중포인터를 매개변수로 받아도 해제할 수 있을 것 */
+	 	 - initialize와 왜 다른지 이해 할것
+	 	 - 이중포인터를 매개변수로 받아도 해제할 수 있을 것 */
 int freeList(headNode* h);
 
 int insertNode(headNode* h, int key);
@@ -57,7 +57,7 @@ int main()
 	headNode* headnode=NULL;
 
 	printf("[----- [한민우] [2018038047] -----]\n");
-    
+	 
 	do{
 		printf("----------------------------------------------------------------\n");
 		printf("                     Doubly Linked  List                        \n");
@@ -71,43 +71,52 @@ int main()
 
 		printf("Command = ");
 		scanf(" %c", &command);
-
-		switch(command) {
+		switch (command) {
+			/* 초기화 */
 		case 'z': case 'Z':
 			initialize(&headnode);
 			break;
+			/* 리스트 출력 */
 		case 'p': case 'P':
 			printList(headnode);
 			break;
+			/* 오름차 순으로 노드 삽입 */
 		case 'i': case 'I':
 			printf("Your Key = ");
 			scanf("%d", &key);
 			insertNode(headnode, key);
 			break;
+			/* 원하는 노드 삭제 */
 		case 'd': case 'D':
 			printf("Your Key = ");
 			scanf("%d", &key);
 			deleteNode(headnode, key);
 			break;
+			/* 맨 뒤에 노드 삽입 */
 		case 'n': case 'N':
 			printf("Your Key = ");
 			scanf("%d", &key);
 			insertLast(headnode, key);
 			break;
+			/* 맨 뒤 노드 삭제 */
 		case 'e': case 'E':
 			deleteLast(headnode);
 			break;
+			/* 맨 앞에 노드 삽입 */
 		case 'f': case 'F':
 			printf("Your Key = ");
 			scanf("%d", &key);
 			insertFirst(headnode, key);
 			break;
+			/* 맨 앞 노드 삭제 */
 		case 't': case 'T':
 			deleteFirst(headnode);
 			break;
+			/* 리스트 역순으로 만들기 */
 		case 'r': case 'R':
 			invertList(headnode);
 			break;
+			/* 종료 */
 		case 'q': case 'Q':
 			freeList(headnode);
 			break;
@@ -121,13 +130,18 @@ int main()
 	return 1;
 }
 
-
+/* 이중 포인터를 통해 headNode의 주소값을 직접 받아서 h의 값을 바로 바꾼다.*/
 int initialize(headNode** h) {
-
+	/* headNode가 NULL이 아니면, freeNode를 호출하여 할당된 메모리 모두 해제 */
+	if (*h != NULL)
+		freeList(*h);
+	
+	*h = (headNode*)malloc(sizeof(headNode)); // headNode의 포인터에 동적 할당 해준다.
+	(*h)->first = NULL; // first를 초기화 시켜준다.
 	return 1;
 }
 
-int freeList(headNode* h){
+int freeList(headNode* h) {
 	return 0;
 }
 
@@ -161,7 +175,21 @@ void printList(headNode* h) {
  * list에 key에 대한 노드하나를 추가
  */
 int insertLast(headNode* h, int key) {
-
+	listNode* node = (listNode*)malloc(sizeof(listNode));
+	listNode* Toend;
+	node->key = key;
+	node->rlink = NULL;
+	Toend = h->first;
+	if(h->first == NULL) { // 리스트가 공백상태면 맨 앞에 node 삽입
+		h->first = node;
+	}
+	else {
+		while(Toend->rlink != NULL) { // 리스트 끝까지 이동
+			Toend = Toend->rlink;
+		}
+		Toend->rlink = node; // 맨 뒤에 삽입
+		node->llink = Toend; // llink 연결
+	}
 	return 0;
 }
 
